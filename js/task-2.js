1b5b7861df9503c2ff5797435f6aa41520b93221
Good worker <bankoff1980@gmail.com>
2024-06-15 22:49:10 +0300

    add index file

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+function formatMessage(message, maxLength) {
+    const mLength1 = message.length;
+    const mLength2 = maxLength.length;
+    if (mLength1 <= mLength2) {
+        return message
+    }
+    else {
+        return `${mLength1}...`
+    }
+}
+
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
